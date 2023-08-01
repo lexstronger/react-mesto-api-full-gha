76cd1f419a523ffe710f085da2bad2aafd8aa351
Alex Vysochin <stronger00@mail.ru>
2023-08-01 22:13:58 +0300

    Вернул пакет сors и исправил gitignore

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
+const cors = require('cors');
 const router = require('./routes/routers');
 const centralHandlerError = require('./middlewares/centralHadlerError');
 
 const { PORT = 3001 } = process.env;
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
