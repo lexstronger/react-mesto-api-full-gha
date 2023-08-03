4f9ab83e3e22b9e2356d38022a3f0d71c6f7b610
Alex Vysochin <stronger00@mail.ru>
2023-08-03 20:24:56 +0300

    Пробую заново настроить сервер

--- backend/middlewares/centralHadlerError.js
+++
@@ -1,9 +0,0 @@
-const centralHandlerError = (err, req, res, next) => {
-  if (!err.statusCode) {
-    return res.status(500).send({ message: 'Непредусмотренная ошибка' });
-  }
-  res.status(err.statusCode).send({ message: err.message });
-  return next();
-};
-
-module.exports = centralHandlerError;
--- backend/utils/constants.js
+++
@@ -1,6 +0,0 @@
-const SUCCESS = 200;
-const CREATED = 201;
-
-module.exports = {
-  SUCCESS, CREATED,
-};
